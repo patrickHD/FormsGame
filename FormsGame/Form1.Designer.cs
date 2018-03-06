e2ba19ea5aeab1874291131ca39d4cb97180e30a
patrickHD <patrickhdaniel@outlook.com>
2018-03-05 23:58:23 -0600

    Added input handeling tests

--- FormsGame/Form1.Designer.cs
+++ FormsGame/Form1.Designer.cs
@@ -29,11 +29,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblKeys = new System.Windows.Forms.Label();
+            this.box = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.lblBulletCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKeys
+            // 
+            this.lblKeys.AutoSize = true;
+            this.lblKeys.Location = new System.Drawing.Point(12, 9);
+            this.lblKeys.Name = "lblKeys";
+            this.lblKeys.Size = new System.Drawing.Size(33, 13);
+            this.lblKeys.TabIndex = 0;
+            this.lblKeys.Text = "Keys:";
+            // 
+            // box
+            // 
+            this.box.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.box.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.box.Location = new System.Drawing.Point(343, 166);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(50, 50);
+            this.box.TabIndex = 1;
+            this.box.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(12, 35);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(41, 13);
+            this.lblSpeed.TabIndex = 2;
+            this.lblSpeed.Text = "Speed:";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(12, 22);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(54, 13);
+            this.lblLocation.TabIndex = 3;
+            this.lblLocation.Text = "Location: ";
+            // 
+            // lblBulletCount
+            // 
+            this.lblBulletCount.AutoSize = true;
+            this.lblBulletCount.Location = new System.Drawing.Point(12, 48);
+            this.lblBulletCount.Name = "lblBulletCount";
+            this.lblBulletCount.Size = new System.Drawing.Size(67, 13);
+            this.lblBulletCount.TabIndex = 4;
+            this.lblBulletCount.Text = "Bullet Count:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(752, 393);
+            this.Controls.Add(this.lblBulletCount);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.lblKeys);
+            this.Name = "Form1";
+            this.Text = "Game";
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblKeys;
+        private System.Windows.Forms.PictureBox box;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label lblBulletCount;
     }
 }
 
--- FormsGame/Form1.cs
+++ FormsGame/Form1.cs
@@ -12,9 +12,121 @@ namespace FormsGame
 {
     public partial class Form1 : Form
     {
+        List<string> keysdown = new List<string>();
+        List<PictureBox> bullets = new List<PictureBox>();
+        int speed = 5;
+        int bcnt = 0;
         public Form1()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(OnKeyDown);
+            this.KeyUp += new KeyEventHandler(OnKeyUp);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblSpeed.Text = "Speed: " + speed.ToString();
+            lblLocation.Text = "Location: " + box.Location.X.ToString() + ", " + box.Location.Y.ToString();
+            lblBulletCount.Text = "Bullet Count: " + bullets.Count;
+            if (keysdown.Contains(Keys.Up.ToString()))
+            {
+                box.Location = new Point(box.Location.X, box.Location.Y - speed);
+            }
+            if (keysdown.Contains(Keys.Down.ToString()))
+            {
+                box.Location = new Point(box.Location.X, box.Location.Y + speed);
+            }
+            if (keysdown.Contains(Keys.Left.ToString()))
+            {
+                box.Location = new Point(box.Location.X - speed, box.Location.Y);
+            }
+            if (keysdown.Contains(Keys.Right.ToString()))
+            {
+                box.Location = new Point(box.Location.X + speed, box.Location.Y);
+            }
+            if (keysdown.Contains(Keys.Z.ToString()))
+            {
+                box.BackColor = Color.Red;
+            }
+            else if (keysdown.Contains(Keys.X.ToString()))
+            {
+                box.BackColor = Color.Blue;
+            }
+            else
+            {
+                box.BackColor = Color.Yellow;
+            }
+            if (bcnt >= 10)
+            {
+                bcnt = 0;
+            }
+            else
+            {
+                bcnt++;
+            }
+            if (keysdown.Contains(Keys.Space.ToString()) && (bcnt == 0))
+            {
+                SpawnBullet();
+            }
+            MoveBullets();
+            
+        }
+
+        public void SpawnBullet()
+        {
+            PictureBox bullet = new PictureBox
+            {
+                Anchor = (AnchorStyles.Bottom | AnchorStyles.Left),
+                Location = new Point(box.Location.X, box.Location.Y),
+                Size = new Size(25, 25),
+                BackColor = Color.Black
+            };
+            bullets.Add(bullet);
+            Controls.Add(bullet);
+        }
+
+        public void MoveBullets()
+        {
+            if (bullets.Count > 0)
+            {
+                lblSpeed.Text = bullets.Last().Location.Y.ToString();
+                foreach (PictureBox bullet in bullets.ToList())
+                {
+                    bullet.Location = new Point(bullet.Location.X, bullet.Location.Y - 5);
+                    if (bullet.Location.Y < 25)
+                    {
+                        Controls.Remove(bullet);
+                        bullets.Remove(bullet);
+                    }
+                }
+            }
+        }
+        
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!keysdown.Contains(e.KeyCode.ToString()))
+                keysdown.Add(e.KeyCode.ToString());
+            UpdateKeys();
+            if (e.KeyCode == Keys.Space)
+                bcnt = 0;
+        }
+
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (keysdown.Contains(e.KeyCode.ToString()))
+                keysdown.Remove(e.KeyCode.ToString());
+            UpdateKeys();
+        }
+
+        public void UpdateKeys()
+        {
+            lblKeys.Text = "Keys: " + string.Join(", ", keysdown);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
